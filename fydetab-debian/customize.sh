8ea32eb939aef1e75145f926e7e8337488d0d69f
Archie <a627@ArchiedeMac-mini.local>
2025-03-12 15:40:40 +0800

    fix build process

--- fydetab-debian/customize.sh
+++ fydetab-debian/customize.sh
@@ -1,5 +1,3 @@
-#! /bin/bash
-
 chroot=$1
 pkgs="grub-efi-arm64 initramfs-tools linux-headers-6.1.0-1023-rockchip linux-image-6.1.0-1023-rockchip linux-modules-6.1.0-1023-rockchip mali-g610-firmware fydetabduo-post-install kmod systemd-timesyncd"
 run_in_chroot() {
@@ -8,8 +6,23 @@ run_in_chroot() {
 
 echo "Setting up the system"
 
-# deb [trusted=yes] https://deb-mirror.fydeos.com/fydetab/ bookworm main
-echo "deb [trusted=yes] https://deb-mirror.fydeos.com/fydetab/ bookworm main" > $chroot/etc/apt/sources.list.d/fydetab.list
+
+cat <<EOF > $chroot/etc/apt/sources.list
+deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
+EOF
+
+
+cat <<EOF >> $chroot/etc/hosts
+127.0.0.1	debian-ftd
+EOF
+
+run_in_chroot "wget -O - https://deb-mirror.fydeos.com/fydetab/key.gpg | gpg --dearmor -o /usr/share/keyrings/fydetab-archive-keyring.gpg"
+
+# deb [signed-by=/usr/share/keyrings/fydetab-archive-keyring.gpg] https://deb-mirror.fydeos.com/fydetab bookworm main
+echo "deb [signed-by=/usr/share/keyrings/fydetab-archive-keyring.gpg] https://deb-mirror.fydeos.com/fydetab bookworm main" > $chroot/etc/apt/sources.list.d/fydetab.list
+
 
 # Set up APT pinning for the repository
 cat <<EOF > $chroot/etc/apt/preferences.d/fydetab
